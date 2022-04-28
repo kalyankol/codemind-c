2b94af39b8c96710dfaf11f31450b83203d20d30
kalyankol <103946030+kalyankol@users.noreply.github.com>
2022-04-28 20:43:21 +0530

    #include<stdio.h> int main() { 	int a[100],b[100],n,c[100],i,j,k; 	scanf("%d",&n); 	for(i=0;i<n;i++) 	{ 		scanf("%d",&a[i]); 	} 	for(i=0;i<n;i++) 	{ 		scanf("%d",&b[i]); 	} 	for(i=0;i<n;i++) 	{ 		c[i]=a[i]+b[i]; 	} 	for(i=0;i<n;i++) 	{ 		printf("%d ",c[i]); 	} }

---
+++ Monk_and_Welcome_Problem.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int a[100],b[100],n,c[100],i,j,k;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&b[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		c[i]=a[i]+b[i];
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",c[i]);
+	}
+}
