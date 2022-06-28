f93c30747395ac5cfbaf78ff5299c222030cd9ee
kalyankol <103946030+kalyankol@users.noreply.github.com>
2022-06-28 18:11:20 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(i==j || i+j==n+1)             printf("x");             else             printf("0");         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,13 +1,17 @@
+
 #include<stdio.h>
 int main()
 {
-    int n,i,j;
+    int i,j,n;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        for(j=1;j<=i;j++)
+        for(j=1;j<=n;j++)
         {
-            printf("%d",j);
+            if(i==j || i+j==n+1)
+            printf("x");
+            else
+            printf("0");
         }
         printf("
 ");
