2bbbca614a49dbeeb18cb2106c3509e261c87a94
kalyankol <103946030+kalyankol@users.noreply.github.com>
2022-06-28 21:03:35 +0530

    #include<stdio.h> int main() {     int i,n,s1=0,k;     scanf("%d",&n);     int arr[n];     for(i=0;i<=n;i++)     {         scanf("%d",&arr[i]);         if(arr[i]%2!=0)         {             k=i;             break;         }     }     for(i=0;i<k;i++)     {         s1=s1+arr[i];     }     printf("%d",s1); }

---
+++ sum_of_array__elements_upto__first_odd_number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,s1=0,k;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<=n;i++)
+    {
+        scanf("%d",&arr[i]);
+        if(arr[i]%2!=0)
+        {
+            k=i;
+            break;
+        }
+    }
+    for(i=0;i<k;i++)
+    {
+        s1=s1+arr[i];
+    }
+    printf("%d",s1);
+}
